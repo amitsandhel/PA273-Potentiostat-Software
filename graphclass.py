65c593675b27f91a7a2214679f57821ef676533a
Fredrick Leber <fredrickleber@gmail.com>
2015-06-30 14:49:07 -0400

    PEP8 and Bugfix
    -Added PEP8 Compatibility for main, v1, graphclass, fake_serial, and
    postrun
    -Fixed bug with main.py. Previously it didn't run on UNIX/Windows 8
    -Removed Redundant Code

--- graphclass.py
+++ graphclass.py
@@ -1,112 +1,99 @@
-#!/usr/bin/python
-# encoding: utf-8
-#graphclass.py
-'''
-Created by Amit Sandhel.
-This script is designed to display the graph in real time
-'''
+import logging
 
-"""
+# !/usr/bin/python
+# encoding: utf-8
+# graphclass.py
+
+""" Created by Amit Sandhel.
+This script is designed to display the graph in real time
+
 Note: this program requires:
         1) Python 2.7
         2) Matplotlib
         3) NumPy
-        4) SciPy
-        4) math-built in to Pyton
-        5) logging-built in to Python
+        4) logging-built in to Python
 """
-import logging
-'''Setting up logging'''
-logging.basicConfig(filename='graphclass.log', filemode='a', level=logging.DEBUG, format='%(asctime)s, %(levelname)s, %(message)s')
-logging.info(" ---------------------- root (%s) --------------------------------" % __file__)
 
-#name for log file
+# Setting up logging
+logging.basicConfig(filename='graphclass.log', filemode='a',
+                    level=logging.DEBUG, format='%(asctime)s, \
+                    %(levelname)s, %(message)s')
+logging.info(" ---------------------- root (%s) \
+             --------------------------------" % __file__)
+
+# name for log file
 logger = logging.getLogger('graphclass.log')
 
 '''Testing to ensure all libraries exist on computer'''
 
-#Does matplotlib exist on library 
+# Does matplotlib exist on library
 GRAPH = True
 try:
     import matplotlib.pyplot as plt
-    
+
 except:
     GRAPH = False
-    logger.debug('Error: Please install Matplotlib and Numpy')
-
-#Does numpy library exist
-NUMPY = True
-try:
-    #importing numpy library
-    from numpy import * 
-except:
-    NUMPY = False
-    #print statement to let user know to install properly libraries
-    logger.debug('Error: Please install Numpy and SciPy')
-    sys.exit(0)
-
-from datetime import datetime
-
-
+    logger.debug('Error: Please install Matplotlib.')
 
 
 class GraphClass():
-    '''takes the data and displayst the data in real time
-    '''
-    def __init__(self, data = None, data2 = None, data3 = None):
-        #various arg pass in arguments
+    """Takes the data and displays it in real time."""
+    def __init__(self, data=None, data2=None, data3=None):
+        # various arg pass in arguments
         self.data = data
         self.data2 = data2
-        self.data3= data3
-        
-        #setting up figure variables
+        self.data3 = data3
+
+        # setting up figure variables
         self.fig = plt.figure(1)
-        self.ax1 = plt.subplot(2,1,1)
-        self.ax2 = plt.subplot(2,1,2)
-        
-        #lists to access data 
-        self.time_list1=[]
-        self.bias_list2=[]
-        self.current_list3=[]
-    
+        self.ax1 = plt.subplot(2, 1, 1)
+        self.ax2 = plt.subplot(2, 1, 2)
+
+        # lists to access data
+        self.time_list1 = []
+        self.bias_list2 = []
+        self.current_list3 = []
+
     def analysis(self, data, data2, data3):
-        '''appends the data obtained from beastie into individual lists and then graphs the last 5 elements of each list'''
-        self.time_list1.append( data )
-        self.bias_list2.append( data2 )
-        self.current_list3.append( data3)
-        
-        #call graph function and graphs last 5 elements of each list
-        self.graph(self.time_list1[-5:], self.bias_list2[-5:], self.current_list3[-5:])
-            
-    def graph(self, n_d_time, n_d1_bias, n_d2_current): 
-        '''graph function takes parameters for graphing'''
-        #n_d = time
-        #n_d1 = bias
-        #n_d2 = current
-        #clear the axis each iteration
+        """appends the data obtained from beastie into individual lists and
+        then graphs the last 5 elements of each list
+        """
+        self.time_list1.append(data)
+        self.bias_list2.append(data2)
+        self.current_list3.append(data3)
+
+        # call graph function and graphs last 5 elements of each list
+        self.graph(self.time_list1[-5:], self.bias_list2[-5:],
+                   self.current_list3[-5:])
+
+    def graph(self, n_d_time, n_d1_bias, n_d2_current):
+        '''graph function, takes parameters for graphing'''
+        # n_d = time
+        # n_d1 = bias
+        # n_d2 = current
+        # clear the axis each iteration
         self.ax1.cla()
-        
-        #autoscale the graph
-        plt.autoscale(enable = True, axis = 'both', tight = True)
-        
-        #plotting the graph with color
+
+        # autoscale the graph
+        plt.autoscale(enable=True, axis='both', tight=True)
+
+        # plotting the graph with color
         self.ax1.plot(n_d_time, n_d1_bias, "-m")
-        
-        #clear the axis each iteration
+
+        # clear the axis each iteration
         self.ax2.cla()
-        
-        #autoscale the axis
-        plt.autoscale(enable = True, axis = 'both', tight = True)
-        
-        #plotting the graph with color
-        self.ax2.plot( n_d_time, n_d2_current, "-r")
-        
-        #allows us to  graph in "real time" every iteration new plot is displayed
-        plt.show(block = False)
+
+        # autoscale the axis
+        plt.autoscale(enable=True, axis='both', tight=True)
+
+        # plotting the graph with color
+        self.ax2.plot(n_d_time, n_d2_current, "-r")
+
+        # Allows graphing in "real time". Every iteration new plot is displayed
+        plt.show(block=False)
         plt.draw()
-    
-#####################################################################################    
+
+###############################################################################
 if __name__ == '__main__':
     print('Welcome to my Graphclass Graphing Environment!\n')
     GraphClass()
-  
--- pa273_v1.py
+++ pa273_v1.py
@@ -1,104 +1,106 @@
-#!/usr/bin/python
+import sys
+import time
+import logging
+import msvcrt
+from main import setup_parser as sp
+from Fake_Serial import Fake_Serial as fake_serial
+
+# !/usr/bin/python
 # encoding: utf-8
-#pa273_v1.py
+# pa273_v1.py
 
-'''
-Created by Amit Sandhel on 2013-05-27.
-This module is to probe a COM port for the PA273 potentiostat 
+'''Created by Amit Sandhel on 2013-05-27.
+This module is to probe a COM port for the PA273 potentiostat
 
-'''
-
-"""
 Note: this program requires:
         1) Python 2.7
         2) Matplotlib
-        3) NumPy
-        4) SciPy
-        4) math-built in to Pyton
-        5) logging-built in to Python
-        6) PySerial
-"""
+        3) logging-built in to Python
+        4) PySerial
+'''
 
-import sys
-import time
-import logging 
-import subprocess
-
-#import argparse for argparse commands
-import argparse
-
-from main import setup_parser as sp
-import msvcrt
-
-from Fake_Serial import Fake_Serial as fake_serial
-
-#making a logging file
-logging.basicConfig(filename='pa273v1.log', filemode='a', level=logging.DEBUG, format='%(asctime)s, %(levelname)s, %(message)s')
+# making a logging file
+logging.basicConfig(filename='pa273v1.log', filemode='a', level=logging.DEBUG,
+                    format='%(asctime)s, %(levelname)s, %(message)s')
 logging.info(" ---------------------- root --------------------------------")
 
-#name for the log file
+# name for the log file
 logging1 = logging.getLogger('pa273v1.log')
 
 
-#SETTING UP CSV FILE
+# SETTING UP CSV FILE
 FILENAME = "BOOK3.csv"
 NEWLINE = "\n"
 
 '''COM PORT SETTINGS
-Change the com port setting to the com port value desired 
+Change the com port setting to the com port value desired
 COM PORT SETTINGS CHANGE
-NOTE: Must be STRING and have the  WORD "COM" in front of it in order to be used by MySerialPort.open_port() function
+NOTE: Must be STRING and have the  WORD "COM" in front of it in order to be
+used by MySerialPort.open_port() function.
 '''
-'''STATIC DATA DO NOT TOUCH BASED ON POTENTIOSTAT MANUAL'''
+
+# STATIC DATA - DO NOT TOUCH! BASED ON POTENTIOSTAT MANUAL'''
 TIMEDELAY = 1
 EIGHTBITS = 8
 PARITY_NONE = 'N'
 STOPBITS_ONE = 1
 
 
-#Change COM PORT AS NEEDED
-#String varuiable with word COM in caplocks in front is a must
-#example: "COM4", "COM2", "COM1", etc...
-COM="COM4" #pick whichever com port your computer will need defaulted to COM4 here
+# Change COM PORT AS NEEDED
+# String varuiable with word COM in caplocks in front is a must
+# example: "COM4", "COM2", "COM1", etc...
+
+# pick whichever com port your computer will need defaulted to COM4 here
+COM = "COM4"
 
 
-#define the ports you have available
+# define the ports you have available
 def list_ports():
     print "COM4"
-    return ["COM4",]
+    return ["COM4", ]
+
 
 class MySerialPort():
-    '''potentiostat class that reads the command file, runs the command file from a serial port 
-    '''
+    """Potentiostat class that reads the command file, and runs the command
+    file from a serial port.
+    """
     def __init__(self):
-        # initializing the variable so all functions can access the self.s port and remain open
+        '''Initializing the variable so all functions can access the
+        self.s port and remain open.
+        '''
         self.s = None
-    
+
     def my_ports():
         '''Call tool to detect and list all serial ports'''
         return list_ports.main()
-        
-    def open_port(self, port=COM, baudrate=19200, bytesize=EIGHTBITS, parity=PARITY_NONE,
-                            stopbits=STOPBITS_ONE, timeout=1, xonxoff=False, rtscts=False,
-                            writeTimeout=3, dsrdtr=False, interCharTimeout=None):
-        '''Open a self.s port, explicitly define all values to be sure they are set'''
-        # Notes I set a default read and write timeout of 1 seconds to get it to respond quickly 
-        
-        s = Serial(port, baudrate, bytesize, parity, stopbits, timeout, xonxoff, rtscts,
-                            writeTimeout, dsrdtr, interCharTimeout)
+
+    def open_port(self, port=COM, baudrate=19200, bytesize=EIGHTBITS,
+                  parity=PARITY_NONE, stopbits=STOPBITS_ONE, timeout=1,
+                  xonxoff=False, rtscts=False, writeTimeout=3, dsrdtr=False,
+                  interCharTimeout=None):
+        '''Open a self.s port, explicitly define all values to be sure
+        they are set. Notes: I set a default read and write timeout of
+        1 second to get it to respond quickly.
+        '''
+        s = Serial(port, baudrate, bytesize, parity, stopbits, timeout,
+                   xonxoff, rtscts, writeTimeout, dsrdtr, interCharTimeout)
         self.s = s
 
     def send(self, str_to_send):
         """sending commands to the serial port """
         chars_sent = self.s.write(str_to_send)
-        logging1.debug("Tx: " + repr(str_to_send) + "bytes sent: %d" % chars_sent)
+        logging1.debug("Tx: " + repr(str_to_send) +
+                       "bytes sent: %d" % chars_sent)
         return chars_sent
-    
+
     def receive(self, max_chars):
-        '''Receive string with timeout and wait for end-of-message character or error'''
+        '''Receive string with timeout and wait for end-of-message
+        character or error
+        '''
         data_string = ""
         start_time = time.time()
-        MAXRECEIVETIMEOUT = 3  # is this in seconds or milli seconds?  Look up time.now()
+        # Are units seconds or milliseconds? Look up time.now()
+        MAXRECEIVETIMEOUT = 3
         while True:
             b = self.s.inWaiting()
             if b > 0:
@@ -106,21 +108,26 @@ class MySerialPort():
                 if new_char == "*":
                     logging1.debug("Rx -  * received")
                     break
-                    
+
                 elif new_char == "\r" or new_char == "\n":
-                    # watch for other special characters like "\r \f" review your logs to see if anything else is embedded.
-                    pass 
+                    '''watch for other special characters like "\r \f" review
+                    your logs to see if anything else is embedded.
+                    '''
+                    pass
                 else:
                     data_string = data_string + new_char
             if time.time() - start_time > MAXRECEIVETIMEOUT:
-                logging1.debug("Rx Receive timeout, returning what I have and hoping")
+                logging1.debug("Rx Receive timeout, returning what \
+                I have and hoping")
                 break
         time.sleep(0.01)
-                #logging1.debug("Rx: " + repr(data_string) + " bytes read: %d" % len(data_string))
+        '''logging1.debug("Rx: " + repr(data_string) +
+                          " bytes read: %d" % len(data_string))
+        '''
         if new_char == "?":
             logging1.debug("Rx - Error recieve, now what?")
         return data_string
-    
+
     def close_port(self):
         '''closes the self.s port'''
         self.s.close()
@@ -132,11 +139,11 @@ class MySerialPort():
         '''
         print '(1:--> X1; 5:->X5; 10:-->X10; 50:-->X50)'
         x = input('ENTER EGAIN VALUE: ')
-        self.send("EGAIN %s  \n" %x)
+        self.send("EGAIN %s  \n" % x)
         reply = self.receive(12)
         print reply
         return reply
-        
+
     def igain(self):
         '''setting current measurement ahead of A-to-D converter
         f.s. = functional sensitivity
@@ -144,90 +151,91 @@ class MySerialPort():
         '''
         print '(1:--> X1; 5:->X5; 10:-->X10; 50:-->X50)'
         x = input('ENTER IGAIN VALUE: ')
-        self.send("IGAIN %s  \n" %x)
+        self.send("IGAIN %s  \n" % x)
         reply = self.receive(12)
         print reply
         return reply
 
     def bias(self):
-        '''setting the bias potential as soon as the cell is on
-        PAGE 15  BIAS[]
+        '''Setting the bias potential as soon as the cell is on.
+        Reference: PAGE 15  BIAS[]
         '''
         x = input('Enter the Potential Bias to apply (mV): ')
-        self.send('BIAS %s \n'%x)
-        reply = self.receive(20) #13 AT MAX VALUE
+        self.send('BIAS %s \n' % x)
+        reply = self.receive(20)  # 13 AT MAX VALUE
         print reply
         return reply
-        
+
     def measure_potential(self):
-        '''running the potentiostat to apply potential and measure the 
-        current and then return the value directly to the computer
-        Note that each command is ran in single order due to ease and safety of command processing 
-        the data is then saved into a dictionary directly and no if loop is required to check for error
-        however do note that two errors do exist
-        Also note that all these commands are READ commands only no writing is done in this function
+        '''Running the potentiostat to apply potential and measure the current,
+        and then return the value directly to the computer. Note that each
+        command is ran in single order due to ease and safety of command
+        processing. The data is then saved into a dictionary directly, no
+        if loop is required to check for error. However, note that two errors
+        do exist. Also, note that all these commands are READ commands only; no
+        writing is done in this function.
         '''
         self.send('EGAIN \n')
         a = self.receive(12)
-        self.egainval=a.strip()
-        
-        
+        self.egainval = a.strip()
+
         self.send('IGAIN \n')
-        b =self.receive(12)
-        self.igainval=b.strip()
-        
-        
+        b = self.receive(12)
+        self.igainval = b.strip()
+
         self.send('AS \n')
         c = self.receive(12)
-        self.asval=c.strip()
-        
-        
+        self.asval = c.strip()
+
         self.send('SIE 1; A/D \n')
         f = self.receive(25)
-        self.adval=f.strip()
-      
+        self.adval = f.strip()
+
         self.send('BIAS \n')
         g = self.receive(25)
-        self.seteval=g.strip()
-        
-        #uncomment the lines below to access the command measures resistance 
-        #This command is not built in the simulator
-        #self.send('Q \n')
-        #h = self.receive(17)
-        #self.qval=h.strip()
-        
+        self.seteval = g.strip()
+
+        # uncomment the lines below to access the command measures resistance
+        # This command is not built in the simulator
+        # self.send('Q \n')
+        # h = self.receive(17)
+        # self.qval=h.strip()
 
     def record_data(self):
-        '''this is to record the data output into a csv file with the timestamp'''
+        '''Records the data output into a csv file with the timestamp'''
         myfile = open(FILENAME, 'a')
         newrow = time.strftime('%H:%M:%S,')
-        newrow += str(self.seteval) + "," #applied potential
-        newrow += str(self.egainval) + "," #EGAIN SETTING
-        newrow += str(self.igainval) + "," #IGAIN SETTING
-        newrow += str(self.asval) + "," #CURRENT RANGE
-        #newrow += str(self.eppval) + "," #APPLIED POTENTIAL READOUT
-        newrow += str(self.adval) + "," #CURRENT READOUT
-        #newrow += str(self.qval)  #CHARGE READOUT
+        newrow += str(self.seteval) + ","  # applied potential
+        newrow += str(self.egainval) + ","  # EGAIN SETTING
+        newrow += str(self.igainval) + ","  # IGAIN SETTING
+        newrow += str(self.asval) + ","  # CURRENT RANGE
+        # newrow += str(self.eppval) + ","  # APPLIED POTENTIAL READOUT
+        newrow += str(self.adval) + ","  # CURRENT READOUT
+        # newrow += str(self.qval)  # CHARGE READOUT
         newrow += NEWLINE
         myfile.write(newrow)
         myfile.close()
-    
+
     def run(self):
-        '''main while loop that controls, runs and executes all other commands'''
-        '''running everything in this main excution function'''
+        '''main while loop that controls, runs and executes all other commands
+           running everything in this main excution function
+           '''
         cycles = 1
-        
+
         self.open_port()
         self.egain()
         self.igain()
         self.bias()
-        
-        # changed from 'w' to 'a' to append files indefinitely to preexisting file
+
+        '''changed from 'w' to 'a' to append files indefinitely to
+        preexisting file
+        '''
         myfile = open(FILENAME, "a")
         myfile.write("new data" + NEWLINE)
-        myfile.write("Time, BIAS, EGAIN, IGAIN, I-RANGE, Eapp_READOUT, Current_Readout, CHARGE(Q), Qexp" + NEWLINE)
+        myfile.write("Time, BIAS, EGAIN, IGAIN, I-RANGE, Eapp_READOUT, \
+        Current_Readout, CHARGE(Q), Qexp" + NEWLINE)
         myfile.close()
-    
+
         first_run = True
         while True:
             print "cycle", cycles
@@ -235,69 +243,70 @@ class MySerialPort():
             self.measure_potential()
             self.record_data()
             time.sleep(TIMEDELAY)
-            #making a keyboard excution stop loop
+            # making a keyboard excution stop loop
             if msvcrt.kbhit():
                 ch = msvcrt.getch()
                 if ch.upper() == 'A' or first_run:
                     first_run = False
-                    #re-running function commands 
+                    # re-running function commands
                     self.egain()
                     self.igain()
                     self.bias()
                     myfile = open(FILENAME, "a")
                     myfile.write("NEW DATA" + NEWLINE)
-                    myfile.write("Time, BIAS, EGAIN, IGAIN, I-RANGE, Eapp_READOUT, Current_Readout, CHARGE(Q), Qexp" + NEWLINE)
+                    myfile.write("Time, BIAS, EGAIN, IGAIN, I-RANGE, \
+                    Eapp_READOUT, Current_Readout, CHARGE(Q), Qexp" + NEWLINE)
                     myfile.close()
-                    
-                #to quit the program in all entirely 
+
+                # to quit the program in all entirely
                 elif ch.upper() == "Q":
                     self.close_port()
                     break
 
-        
+
 module = sys.modules[__name__]
+
 
 class Main():
     """Main class which executes the entire pa273_v1 script"""
     def __init__(self, parser):
         args = parser.parse_args()
-        self.sim = args.sim #sim parameter
-    
+        self.sim = args.sim  # sim parameter
+
         self.myfile = MySerialPort()
-        
+
     def run(self):
-        if self.sim == True:
-            logging1.debug("FAKE/VIRTUAL SIM VALUE: " + repr(self.sim) )
+        if self.sim is True:
+            logging1.debug("FAKE/VIRTUAL SIM VALUE: " + repr(self.sim))
             print 'running sim: ', self.sim
-            
-            '''calling fake serial function opening serial port in simulator class only'''
-            setattr(module, "Serial", fake_serial) #Fake_Serial)
+
+            '''calling fake serial function opening serial port in
+            simulator class only
+            '''
+            setattr(module, "Serial", fake_serial)  # Fake_Serial
             self.myfile.open_port()
-            self.myfile.run() 
-            
+            self.myfile.run()
+
             '''closing serial port after run'''
             self.myfile.close_port()
-            
-        if self.sim == False:
-            logging1.debug("REAL SIM VALUE: " + repr(self.sim) )
-            
+
+        if self.sim is False:
+            logging1.debug("REAL SIM VALUE: " + repr(self.sim))
+
             print 'real sim result: ', self.sim
-            
+
             '''running real simulator function which imports serial'''
-            from serial import Serial, EIGHTBITS, PARITY_NONE, STOPBITS_ONE 
-            
+            from serial import Serial
+
             setattr(module, "Serial", Serial)
             self.myfile.open_port()
-            self.myfile.run() 
-            
+            self.myfile.run()
+
             '''closing serial port after run'''
             self.myfile.close_port()
-        
-            
-            
-#####################################################################################  
+
+##############################################################################
 if __name__ == '__main__':
     parser = sp()
-    b= Main(parser)
-    b.run()  
- 
+    b = Main(parser)
+    b.run()
